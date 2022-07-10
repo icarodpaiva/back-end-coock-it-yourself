c3b007420dde7d1dacd786c5782592c5ba59c12c
icaro paiva <icaro.paiva@acct.global>
2022-07-10 00:05:06 -0300

    prevent cors policy

--- src/server.js
+++ src/server.js
@@ -17,6 +17,20 @@ db.once("open", () => console.log("conected"))
 
 server.use(express.json())
 server.use(express.urlencoded({ extended: true }))
+server.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Credentials", true)
+  res.header("Access-Control-Allow-Origin", req.headers.origin)
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE")
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token,Authorization"
+  )
+  if (req.method === "OPTIONS") {
+    return res.status(200).end()
+  } else {
+    next()
+  }
+})
 
 server.use(router)
 
