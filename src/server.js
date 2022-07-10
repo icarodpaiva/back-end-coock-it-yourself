500497a04be983cb03ddaa7beceae040ab9db0cf
icaro paiva <icaro.paiva@acct.global>
2022-07-09 23:15:59 -0300

    adjust in procfile and user db

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT ?? 2525
 const server = express()
 
 mongoose.connect(
-  "mongodb+srv://icaropaiva:deyckson1@cockityourself.udn8j.mongodb.net/?retryWrites=true&w=majority",
+  "mongodb+srv://admin:WGFgvkQQDp24m0hg@cockityourself.udn8j.mongodb.net/?retryWrites=true&w=majority",
   {
     useNewUrlParser: true
   }
@@ -17,20 +17,6 @@ db.once("open", () => console.log("conected"))
 
 server.use(express.json())
 server.use(express.urlencoded({ extended: true }))
-server.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Credentials", true)
-  res.header("Access-Control-Allow-Origin", req.headers.origin)
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE")
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token,Authorization"
-  )
-  if (req.method === "OPTIONS") {
-    return res.status(200).end()
-  } else {
-    next()
-  }
-})
 
 server.use(router)
 
